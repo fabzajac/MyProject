edeb28975352698d2a9d7984a18e8637be038301
Fabian <fabianzajacf@gmail.com>
2020-06-05 13:58:04 +0200

    Initial Commit: Fresh Project

---
+++ src/main/java/com/example/zejdzak/ZejdzakApplication.java
@@ -0,0 +1,13 @@
+package com.example.zejdzak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZejdzakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZejdzakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zejdzak/ZejdzakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zejdzak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZejdzakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
